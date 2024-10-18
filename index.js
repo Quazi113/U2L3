32e13af722afe0585b1c06fbf6b6d5cd1ec737bc
unknown <BT_3W2_08@BKTECH.LOCAL>
2024-10-18 15:35:48 -0400

    HI

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 let count = 0
-
+let pre_count = 0;
 
 let t = document.createElement("title")
 t.innerHTML = "Counter"
@@ -11,3 +11,21 @@ function increment() {
     count = count + 1
     document.getElementById("count-el").innerText = count
 }
+
+function save() {
+    document.getElementById("entries").innerHTML += count + "-"
+    if (pre_count != 0) {
+    let percent = Math.round((count - pre_count) / pre_count * 10000) / 100
+    document.getElementById("percent").innerHTML = "Percent Increase from Last Entry: " + percent + "%"
+    }
+    pre_count = count;
+}
+
+function reset() {
+    count = 0
+    pre_count=0
+    document.getElementById("entries").innerHTML = "Previous entries: "
+    document.getElementById("count-el").innerText = count
+    document.getElementById("percent").innerHTML = "Percent Increase from Last Entry: 0%"
+}
+
